6d8cbb7942cb9ca092bb0619f8d8bf1bf0df116d
Aluno <aluno@laboratorio.int>
2021-02-08 20:29:09 -0300

    avançando com os controllers

--- helpers.js
+++ helpers.js
@@ -1,5 +1,6 @@
 exports.titulopadrao = 'Site Teste';
 exports.menu = [
     {page:'home', slug:'/'},
-    {page:'posts',slug:'/posts'}    
+    {page:'posts',slug:'/posts'},
+    {page:'login',slug:'/users/login'}
 ]
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,11 @@
 const express = require('express');
+const homeController = require('../controllers/homeController');
+const postsController = require('../controllers/postsController');
 
 
 const router = express.Router();
-router.get('/',(req, res)=>{
-    let obj = {
-        title: 'Home',
-        nome: req.query.nome,
-        idade: req.query.idade,
-        mostrar:true,
-        ingredientes:[
-        ],
-        interesses:[
-            'php',
-            'node',
-            'js',
-            'css'
-        ]
-    };
-res.render('home', obj);
-
-});
-router.get('/posts', (req, res)=>{
-    let obj = {
-        title:'Posts',
-        post:
-        [{titulo: 'Post01', conteudo: 'Este é um post sobre nada', id:'1', comentario:'Muito legal'},
-        {titulo: 'Post02', conteudo: 'Este é um segundo post sobre nada também', id:'2', comentario:'Esse foi ruim'}]
-    };
-res.render('posts',obj);
-});
-
-router.get('/:id/post', (req, res)=>{
-let obj = {
-    
-    id: req.params.id,
-    comentario: req.query.coments,
-    title: "Post "+req.params.id
-}
-    res.render('postId',obj);
-});
+router.get('/', homeController.index);
+router.get('/posts',postsController.posts );
+router.get('/:id/post', postsController.postId);
 
 module.exports = router;
